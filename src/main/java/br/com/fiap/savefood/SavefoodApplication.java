6578ec4d1c9332f4701b281e27d3baabcca40683
ViniciusMazinetti <vinicius_mazinetti@hotmail.com>
2021-11-08 16:04:36 -0300

    Projeto montado

---
+++ src/main/java/br/com/fiap/savefood/SavefoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.savefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavefoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavefoodApplication.class, args);
+	}
+
+}
