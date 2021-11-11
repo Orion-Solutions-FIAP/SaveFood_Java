a3e260ce62ccb4ff13d9efed3eb0480c0147723f
ViniciusMazinetti <vinicius_mazinetti@hotmail.com>
2021-11-11 03:55:56 -0300

    Listagem dos produtos por ordem implementados

--- src/main/java/br/com/fiap/savefood/controller/ProductController.java
+++ src/main/java/br/com/fiap/savefood/controller/ProductController.java
@@ -1,27 +1,46 @@
 package br.com.fiap.savefood.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.fiap.savefood.model.Product;
+import br.com.fiap.savefood.model.ProductStatus;
+import br.com.fiap.savefood.model.User;
+import br.com.fiap.savefood.repository.ProductRepository;
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	@GetMapping("/register")
     public String save( Product product ) {
         return "product/form";
     }
 	
-	@GetMapping("/update")
+	@GetMapping("/update/{id}")
     public String update( Product product ) {
         return "product/updateForm";
     }
 	
+	@GetMapping("/delete/{id}")
+	public String delete(Product product) {
+		return "product/updateForm";
+	}
+	
 	@GetMapping()
-    public String products() {
+    public String products(@RequestParam("id") User userLogged, Model model) {
+		List<Product> products = productRepository.findByUserAndStatusOrderByExpirationDateAsc(userLogged, ProductStatus.DISPONIVEL);
+		model.addAttribute("products", products);
+		
         return "product/listAll";
     }
 	
--- src/main/java/br/com/fiap/savefood/repository/ProductRepository.java
+++ src/main/java/br/com/fiap/savefood/repository/ProductRepository.java
@@ -1,9 +1,23 @@
 package br.com.fiap.savefood.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.fiap.savefood.model.Product;
+import br.com.fiap.savefood.model.ProductStatus;
+import br.com.fiap.savefood.model.User;
+
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	public List<Product> findByStatus(ProductStatus status);
+	
+	public List<Product> findByUser(User user);
+	
+	public List<Product> findByUserAndStatus(User user, ProductStatus status);
+	
+	public List<Product> findByUserAndStatusOrderByExpirationDateAsc(User user, ProductStatus status);
 
 }
