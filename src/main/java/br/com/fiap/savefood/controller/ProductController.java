5a56c028ba26f3171fa99ab5f834776d5a5d43bb
ViniciusMazinetti <vinicius_mazinetti@hotmail.com>
2021-11-11 15:57:42 -0300

    Paginação implementada

--- src/main/java/br/com/fiap/savefood/controller/ProductController.java
+++ src/main/java/br/com/fiap/savefood/controller/ProductController.java
@@ -1,8 +1,8 @@
 package br.com.fiap.savefood.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,9 +37,10 @@ public class ProductController {
 	}
 	
 	@GetMapping()
-    public String products(@RequestParam("id") User userLogged, Model model) {
-		List<Product> products = productRepository.findByUserAndStatusOrderByExpirationDateAsc(userLogged, ProductStatus.DISPONIVEL);
+    public String products(@RequestParam("id") User userLogged, @RequestParam(defaultValue="0") int page, Model model) {
+		Page<Product> products = productRepository.findByUserAndStatusOrderByExpirationDateAsc(userLogged, ProductStatus.DISPONIVEL, PageRequest.of(page, 5));
 		model.addAttribute("products", products);
+		model.addAttribute("user", userLogged);
 		
         return "product/listAll";
     }
--- src/main/java/br/com/fiap/savefood/repository/ProductRepository.java
+++ src/main/java/br/com/fiap/savefood/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package br.com.fiap.savefood.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.fiap.savefood.model.Product;
@@ -11,6 +13,8 @@ import br.com.fiap.savefood.model.User;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
 	
-	public List<Product> findByUserAndStatusOrderByExpirationDateAsc(User user, ProductStatus status);
+	public List<Product> findByUser(User user);
+	
+	public Page<Product> findByUserAndStatusOrderByExpirationDateAsc(User user, ProductStatus status, Pageable pageable);
 
 }
